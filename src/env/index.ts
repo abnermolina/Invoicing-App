ede1feab07ce3886f1371b670a0504e7610a8086
Abner Molina <asamuel0416@gmail.com>
2024-05-31 22:00:13 -0700

    changed to production

--- src/env/index.ts
+++ src/env/index.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import * as z from "zod";
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(["dev", "prod", "test"]).default("dev"),
+  NODE_ENV: z.enum(["dev", "production", "test"]).default("dev"),
   PORT: z.coerce.number().default(3000),
   SECRET_JWT: z.string(),
   SECRET_COOKIE: z.string(),
