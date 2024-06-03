92091a565f3b79702aa59cb86d8c6f16840490ec
Abner Molina <asamuel0416@gmail.com>
2024-06-02 20:03:31 -0700

    updated cors origin to localhost from miguel

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ app.register(fastifyMultipart);
 app.register(fastifyFormBody);
 
 app.register(cors, {
-  origin: "origin",
+  origin: "http://localhost:5173",
   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
   credentials: true,
   allowedHeaders: ["content-type"]
