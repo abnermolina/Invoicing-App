46c0503abb5746b9c0c3fc4dbbd78cec26098d79
Abner <asamuel2004@outlook.com>
2024-07-12 21:48:51 -0600

    delete profile photo if user is deleted

--- src/http/controllers/deleteUser.ts
+++ src/http/controllers/deleteUser.ts
@@ -1,5 +1,7 @@
 import { prisma } from "@/lib/prisma";
 import { FastifyReply, FastifyRequest } from "fastify"; //types
+import dotenv from "dotenv";
+import { S3Client, DeleteObjectCommand } from "@aws-sdk/client-s3";
 
 // funtion
 
@@ -7,8 +9,39 @@ export async function deleteUserController(
   req: FastifyRequest, // type to req work from fastify
   res: FastifyReply // type for res to work
 ) {
+  dotenv.config();
+
+  const s3 = new S3Client({
+    credentials: {
+      accessKeyId: process.env.AWS_KEY_ID as string,
+      secretAccessKey: process.env.AWS_SECRET as string,
+    },
+    region: process.env.REGION as string,
+  });
+
   try {
     const userid = req.user.sub;
+
+    const profilePhoto = await prisma.user.findUnique({
+      where: {
+        id: userid,
+      },
+      select: {
+        userProfilePhoto: true,
+      },
+    });
+
+    if (!profilePhoto?.userProfilePhoto) {
+      return res.status(404).send({ message: "No profile photo found" });
+    }
+
+    const params = {
+      Bucket: process.env.PROFILE_PHOTO_S3_BUCKET_NAME as string,
+      Key: profilePhoto?.userProfilePhoto,
+    };
+
+    const command = new DeleteObjectCommand(params);
+    await s3.send(command);
 
     const deletedUser = await prisma.user.delete({
       where: {
